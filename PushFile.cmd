040c1e6b0a24519e3a2ecc37c3527e1ea9f120b7
Kevin Liu <javaandroidc@qq.com>
2024-05-18 09:06:16 +0800

    wiki code by kevin

---
+++ PushFile.cmd
@@ -0,0 +1,9 @@
+
+d:
+cd D:\sourcecode\ecap-NN\ChatGPT\sourcecode\2024\myGithub\wikiecap 
+git add . && git commit -m"update file by keivn" && git push
+gitbook build && start pushfile_copy.cmd
+
+
+
+
